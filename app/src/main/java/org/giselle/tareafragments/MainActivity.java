4ae6159566c7763f04cd32580d37bbba9fd2fca4
Giselle <gisytere@hotmail.com>
2019-10-21 20:20:06 -0500

    Initial commit

---
+++ app/src/main/java/org/giselle/tareafragments/MainActivity.java
@@ -0,0 +1,29 @@
+package org.giselle.tareafragments;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements
+        FragmentOne.OnFragmentInteractionListener {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        FragmentOne fragment = FragmentOne.newInstance();
+        fragmentTransaction.add(R.id.fragmentOne, fragment);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onFragmentInteraction(String text, int colorId) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        FragmentTwo fragment = FragmentTwo.newInstance(text, colorId);
+        fragmentTransaction.replace(R.id.fragmentTwo, fragment);
+        fragmentTransaction.commit();
+    }
+}
